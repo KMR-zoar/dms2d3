cd02d5b094ff25ce8c0d80d383466ac325981abb
Zoar <zoar@k-side.net>
2013-12-19 21:02:50 +0900

    緯度経度の表現を相互に変換するRubyスクリプトです
    角度のDDMMSS.SSSという60進数表記とDD.DDDDDDDという10進数表記を相互に変換するスクリプトです

---
+++ dms2d3.rb
@@ -0,0 +1,34 @@
+#DDMMSS.SSSからDD.DDDDDDDに変換する
+def dms2ddd(dms)
+   num = 0
+   dms = dms.to_s
+   
+   case  dms.to_f.floor.to_s.length
+      when 7
+         num = 2
+      when 6
+         num = 1
+   end
+
+   dd = dms.slice(0 .. 0 + num)
+   mm = dms.slice(1 + num .. 2 + num)
+   ss = dms.slice(3 + num .. dms.length - 1)
+
+   dms2ddd = dd.to_f + mm.to_f / 60 + ss.to_f / 3600
+end
+
+#DD.DDDDDDDからDDMMSS.SSSに変換する
+def ddd2dms(ddd)
+
+   dd = ddd.to_f.floor
+   mm = ((ddd.to_f - dd) * 60)
+   ss = ((mm - mm.floor) * 60)
+
+   if ss.floor.to_s.length == 1
+      sss = "0" + ss.to_s
+   elsif
+      sss = ss.to_s
+   end
+
+   ddd2dms = dd.to_s +  mm.floor.to_s + sss
+end
